66856554db8fc1b883e5cf0f5b76b88d9cfd8113
mutaman97 <mutamanelhadi97@gmail.com>
2024-02-14 06:47:57 +0400

    Fix nqueens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python3
 """Solves the N-queens puzzle.
-
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
-
-Example:
-    $ ./101-nqueens.py N
-
-N must be an integer greater than or equal to 4.
-
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
 """
 import sys
 
 
 def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
+    """Initialize an `n`x`n` sized  chessboard  with 0's."""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
@@ -29,14 +13,14 @@ def init_board(n):
 
 
 def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
+    """ Return a deepcopy of a chesboard."""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
 
 
 def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
+    """ Return the list of lists representation  of a solved chessboard"""
     solution = []
     for r in range(len(board)):
         for c in range(len(board)):
@@ -47,16 +31,7 @@ def get_solution(board):
 
 
 def xout(board, row, col):
-    """X out spots on a chessboard.
 
-    All spots where non-attacking queens can no
-    longer be played are X-ed out.
-
-    Args:
-        board (list): The current working chessboard.
-        row (int): The row where a queen was last played.
-        col (int): The column where a queen was last played.
-    """
     # X out all forward spots
     for c in range(col + 1, len(board)):
         board[row][c] = "x"
