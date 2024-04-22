25b7b9e4e784ef2b48f4ad39c81f9e824494c7e6
mutaman97 <mutamanelhadi97@gmail.com>
2024-04-23 00:17:32 +0400

    fix:  multi language loop

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
-const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-langs.forEach(function(item, index) {
-  console.log(index, item);
-});
+const c = 'C is fun';
+const p = 'Python is cool';
+const j = 'JavaScript is amazing';
+console.log(c);
+console.log(p);
+console.log(j);
