392e70036df478d69332961851cee4bdd5d391a8
mutaman97 <mutamanelhadi97@gmail.com>
2024-04-23 00:19:50 +0400

    fix: spacing again

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const langs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-langs.forEach(function(item, index) {
+langs.forEach(function(item) {
   console.log(item);
 });
