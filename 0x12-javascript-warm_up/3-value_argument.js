fc0ede13755f485fbd1c1f92d374ed44bdd4d586
mutaman97 <mutamanelhadi97@gmail.com>
2024-04-22 23:42:44 +0400

    Fix: value argument once again

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const firstArg = process.argv[2];
 if (firstArg) {
-    console.log(process.argv[2]);
+  console.log(process.argv[2]);
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
