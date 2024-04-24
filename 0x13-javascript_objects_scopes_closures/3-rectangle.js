fcc2f27b60384164dd582e035e42c3a1842d7dbf
mutaman97 <mutamanelhadi97@gmail.com>
2024-04-25 02:17:39 +0400

    Fix: 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,8 @@ class Rectangle {
       //    empty
     }
   }
-  print() {
+
+  print () {
     if (this.width && this.height) {
       for (let i = 0; i < this.height; i++) {
         console.log('X'.repeat(this.width));
